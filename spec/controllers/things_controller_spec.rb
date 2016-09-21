67e6b42ea865a8b65b98f7154d3cde045c55fd8f
fahrenhei7 <fahrenhei7lt@gmail.com>
2016-09-21 20:55:09 +0300

    \#new and \#create actions for authenticated u

--- spec/controllers/things_controller_spec.rb
+++ spec/controllers/things_controller_spec.rb
@@ -7,7 +7,7 @@ describe Web::ThingsController, type: :controller do
       before(:each) { get :index }
 
       it { expect(response).to render_template(:index) }
-      it { expect(assigns(:quests)).to eq(Quest.all) }
+      it { expect(assigns(:thing)).to eq(Thing.all) }
     end
 
     describe 'GET #show' do
@@ -49,6 +49,46 @@ describe Web::ThingsController, type: :controller do
       it 'DELETE #destroy' do
         delete :destroy, params: { id: thing }
         expect(Thing.exists?(thing.id)).to be_truthy
+      end
+    end
+  end
+
+  describe 'authenticated user' do
+    let(:user) { FactoryGirl.create(:user) }
+    before { sign_in(user) }
+    it_behaves_like 'public access to things'
+
+    describe 'GET #new' do
+      before(:each) { get :new }
+      it { expect(response).to render_template(:new) }
+      it { expect(assigns(:thing)).to be_a_new(Thing) }
+    end
+    describe 'POST #create' do
+      context 'valid data' do
+        let(:valid_data) { FactoryGirl.attributes_for(:thing) }
+
+        it 'redirects to thing#show' do
+          post :create, params: { thing: valid_data }
+          expect(response).to redirect_to(thing_path(assigns[:thing]))
+        end
+        it 'creates new record in database' do
+          expect {
+            post :create, params: { thing: valid_data }
+          }.to change(Thing, :count).by(1)
+        end
+      end
+      context 'invalid data' do
+        let(:invalid_data) { FactoryGirl.attributes_for(:thing, title: '') }
+
+        it 'renders :new template' do
+          post :create, params: { thing: invalid_data }
+          expect(response).to render_template(:new)
+        end
+        it 'does not create new record in database' do
+          expect {
+            post :create, params: { thing: invalid_data }
+          }.not_to change(Thing, :count)
+        end
       end
     end
   end
