644becfe85acf4b2f33a7fbe1cb446508ceebfc3
hihihaha <ivan.vekshin@gmail.com>
2016-10-27 23:06:55 +0300

    Removed delivery_now from preview mailer

--- spec/mailers/previews/reminder_mailer_preview.rb
+++ spec/mailers/previews/reminder_mailer_preview.rb
@@ -1,6 +1,6 @@
 # Preview all emails at http://localhost:3000/rails/mailers/reminder_mailer
 class ReminderMailerPreview < ActionMailer::Preview
   def reminder
-    ReminderMailer.reminder(User.first).deliver_now
+    ReminderMailer.reminder(User.first)
   end
 end
